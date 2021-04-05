94a7286e91726cabdcde4abfaca535b9921932d8
idan <42845006+idan-github@users.noreply.github.com>
2021-04-05 13:42:33 +0300

    Update and rename rt_server.lua to s-realtime.lua

---
+++ realtime/s-realtime.lua
@@ -0,0 +1,4 @@
+RegisterNetEvent("realtime:event")
+AddEventHandler("realtime:event", function()
+	TriggerClientEvent("realtime:event", source, tonumber(os.date("%H")), tonumber(os.date("%M")), tonumber(os.date("%S")))
+end)
