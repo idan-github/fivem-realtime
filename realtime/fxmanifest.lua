43cbc15a1e27642f6ed4f1a49197936b6ee5ce9b
idan <42845006+idan-github@users.noreply.github.com>
2021-04-05 13:45:57 +0300

    Update fxmanifest.lua

--- realtime/fxmanifest.lua
+++ realtime/fxmanifest.lua
@@ -1,5 +1,8 @@
 fx_version "cerulean"
 game "gta5"
+author "idan"
+description "change the clock in game to real life clock"
+version "1.0.0"
 
 client_script "c-realtime.lua"
 server_script "s-realtime.lua"
