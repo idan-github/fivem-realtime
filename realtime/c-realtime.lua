c70db5f1e722b42c587a158a21fc9f17a0f5f1e0
idan <42845006+idan-github@users.noreply.github.com>
2021-05-05 14:01:36 +0300

    Update c-realtime.lua

--- realtime/c-realtime.lua
+++ realtime/c-realtime.lua
@@ -1,4 +1,4 @@
-SetMillisecondsPerGameMinute(1000)
+SetMillisecondsPerGameMinute(60000)
 RegisterNetEvent("realtime:event")
 AddEventHandler("realtime:event", function(h, m, s)
 	NetworkOverrideClockTime(h, m, s)
